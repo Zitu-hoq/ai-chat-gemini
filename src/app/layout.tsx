8cf67a416374ae69dfa58a8556b0947de25c3c79
Zitu Hoq <zhzhitu121@gmail.com>
2025-01-03 23:50:36 +0600

    add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Favicon link */}
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
